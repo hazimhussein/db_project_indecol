e4960e65975591cb0a1cb7c55cb2ad5de7f1a3a3
CandyDeck <candy.a.deck@gmail.com>
2023-10-26 13:44:42 +0200

    bootstrap form included

--- app_indecol/views.py
+++ app_indecol/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from . import models
 from django.views import generic
+from django.db.models import Q
+from .models import Project,Person
 
 # Create your views here.
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
 
 def index(request):
     return render(request,'index.html')
@@ -30,42 +35,47 @@ class PersonDetailView(generic.DetailView):
 
 
 
-# def BootstrapFilterView(request):
-#     qs = Project.objects.all()
-#     print('qs',qs)
-    # name_contains_query = request.GET.get('name_contains')
-    # description_contains_query = request.GET.get('description_contains')
-    # date_min = request.GET.get('date_min')
-    # date_max = request.GET.get('date_max')
-    # student = request.GET.get('student')
-    # students= Student.objects.all()
-    # #print(students)
+
+def BootstrapFilterView(request):
+    qs = Project.objects.all()
+    print('qs',qs)
+    name_contains_query = request.GET.get('name_contains')
+    description_contains_query = request.GET.get('description_contains')
+    name_or_description_contains_query = request.GET.get('name_or_description_contains')
+
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
+    person = request.GET.get('person')
+    persons= Person.objects.all()
+    #print(students)
 
 
-    # if name_contains_query != '' and name_contains_query is not None:
-    #     qs = qs.filter(name__icontains=name_contains_query)
+    if name_contains_query != '' and name_contains_query is not None:
+        qs = qs.filter(name__icontains=name_contains_query )
 
 
-    # if description_contains_query != '' and description_contains_query is not None:
-    #     qs = qs.filter(description__icontains=description_contains_query)
+    elif description_contains_query != '' and description_contains_query is not None:
+        qs = qs.filter(description__icontains=description_contains_query)
    
  
+    # elif name_or_description_contains_query != '' and name_or_description_contains_query is not None:
+    #     qs = qs.filter(Q(name__icontains=name_or_description_contains_query) | Q(description_contains_query=name_or_description_contains_query)).distinct()
 
-    # if is_valid_queryparam(date_min) :
-    #     qs = qs.filter(start_date__gte=date_min)
+    if is_valid_queryparam(date_min) :
+        qs = qs.filter(start_date__gte=date_min)
    
-    # if is_valid_queryparam(date_max) :
-    #     qs = qs.filter(start_date__lt=date_max)
+    if is_valid_queryparam(date_max) :
+        qs = qs.filter(start_date__lt=date_max)
 
-    # if is_valid_queryparam(student) and student != 'Choose...':
-    #     print('author',student, students)
-    #     qs = qs.filter(students__last_name=student)
+    if is_valid_queryparam(person) and person != 'Choose...':
+        print('author',person, persons)
+        qs = qs.filter(persons__last_name=person)
      
    
-    # context = {
-    #     'queryset' :qs,
-    #     'students' :students,
+    context = {
+        'queryset' :qs,
+        'persons' :persons,
 
-    # }
+    }
 
-    # return render(request, "bootstrap_form.html",context)
+    return render(request, "bootstrap_form.html",context)
