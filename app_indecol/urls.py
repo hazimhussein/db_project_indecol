5f3bce849a5cce9a465af00e3fd58b60efc12260
Candy Deck <candy.a.deck@gmail.com>
2023-10-27 08:54:37 +0200

    change path projects

--- app_indecol/urls.py
+++ app_indecol/urls.py
@@ -5,7 +5,7 @@ from app_indecol.views import BootstrapFilterView
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('about', views.about, name = 'about'),
-    path('projects', views.projects, name = 'projects'),
+    path('projects/', views.projects, name = 'projects'),
     path('persons', views.persons, name = 'persons'),
     path('project/<str:pk>', views.ProjectDetailView.as_view(), name = 'project_details'),
     path('person/<str:pk>', views.PersonDetailView.as_view(), name = 'person_details'),
