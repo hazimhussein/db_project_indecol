82ddfbbdddc65cceed484922bfdfb12f9ce3a77e
Hazim Hussein <hazimh@ntnu.no>
2023-12-01 11:13:46 +0100

    form validation added

--- frontend/src/components/forms/ProjectForm.jsx
+++ frontend/src/components/forms/ProjectForm.jsx
@@ -66,7 +66,7 @@ function ProjectForm({setData, data}){
 
     let list = useSelector(dataData)
     const current_user = useSelector(authedUser)
-    
+
     let users = list["user"] ? list["user"].filter(user=>user.id!=current_user.id) : []
     let persons = list["person"] ? list["person"] : []
     let partners = list["partner"] ? list["partner"] : []
@@ -84,11 +84,17 @@ function ProjectForm({setData, data}){
       partner:data && data.partners?data.partners.map(partner=>partner.id):[],
       resource:data && data.resources?data.resources.map(resource=>resource.id):[],
       project:data && data.projects ?data.projects.map(project=>project.id):[],
-      start_date:data?dayjs(data.start_date):"",
-      end_date:data?dayjs(data.end_date):""
+      start_date:data?dayjs(data.start_date):null,
+      end_date:data?dayjs(data.end_date):null
     }
 
     const [options, setOptions] = useState(options_dict)
+
+    let invalid_dict = { project_id: false, name: false, description: false, start_date: false, keywords: false, methods: false, persons: false, groups: false}
+
+    const [invalid, setInvalid] = useState(invalid_dict)
+    const [formState, setFormState] = useState("")
+
     const handleSave = () => {
         let cols = ["project_id", "name", "description"]
         let userIds = [...options.user]
@@ -100,8 +106,8 @@ function ProjectForm({setData, data}){
         for (let col of cols){
             entry[col] = document.getElementById(col).value
         }
-        entry["start_date"] = `${options.start_date.$y}-${options.start_date.$M}-${options.start_date.$D}`
-        entry["end_date"] = `${options.end_date.$y}-${options.end_date.$M}-${options.end_date.$D}`
+        entry["start_date"] = options.start_date ? `${options.start_date.$y}-${options.start_date.$M}-${options.start_date.$D}`: options.start_date
+        entry["end_date"] = options.end_date ? `${options.end_date.$y}-${options.end_date.$M}-${options.end_date.$D}` : options.end_date
         entry["type"] = options.type
         entry["keywords"] = typeof options.keyword != "string" ? options.keyword.join(", ") : options.keyword
         entry["methods"] = typeof options.method != "string" ? options.method.join(", ") : options.method
@@ -112,16 +118,51 @@ function ProjectForm({setData, data}){
         entry["projects"] = options.project
         entry["users"] = userIds
         let param = {table:"project", row: entry}
+
         
         if (data == null){
             dispatch(addTableRow(param))
             .then((res)=>{
-                console.log(res)
-                setData({nodes: [...list["project"], res.payload.data]})})
+              if (res.error){
+                Object.keys(invalid).map(key=>{
+                  if (Object.keys(res.payload).includes(key)){
+                    setInvalid(prev=>({...prev, [key]: res.payload[key]}))
+                  } else {
+                    setInvalid(prev=>({...prev, [key]: false}))
+                  }
+                })
+                setFormState("fail")
+            } else {
+              Object.keys(invalid).map(key=>{
+                  setInvalid(prev=>({...prev, [key]: false}))
+              })
+              setFormState("success")
+              setData({nodes: [...list["project"], res.payload.data]})
+            }
+                
+              })
+                
            
         } else {
             dispatch(updateTableRow({table:"project", row: entry, rowId: data.id}))
-            .then((res)=>{setData({nodes: [...list["project"].filter(row=>row.id!=data.id), res.payload.data]});
+            .then((res)=>{
+              if (res.error){
+                Object.keys(invalid).map(key=>{
+                  if (Object.keys(res.payload).includes(key)){
+                    setInvalid(prev=>({...prev, [key]: res.payload[key]}))
+                  } else {
+                    setInvalid(prev=>({...prev, [key]: false}))
+                  }
+                })
+                setFormState("fail")
+            } else {
+              Object.keys(invalid).map(key=>{
+                setInvalid(prev=>({...prev, [key]: false}))
+            })
+              setFormState("success")
+              setData({nodes: [...list["project"].filter(row=>row.id!=data.id), res.payload.data]});
+            }
+            
             })
 
             
@@ -190,37 +231,46 @@ function ProjectForm({setData, data}){
             defaultValue={data && data.project_id}
             autoFocus
             required
+            error = {invalid.project_id && true}
           />
+          {invalid.project_id && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.project_id}</p>}
         <TextField
             label="Full Name"
             id="name"
             className='my-2'
             defaultValue={data && data.name}
             required
+            error = {invalid.name && true}
           />
+          {invalid.name && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.name}</p>}
+
           <Form.Label htmlFor="description">Description</Form.Label>
           <textarea id="description" 
-          className="form-control px-3" 
+          className={`form-control px-3 ${invalid.description && "border-danger"}`} 
           name="description" 
           minLength={10} 
           defaultValue={data && data.description} placeholder={data && data.description}/>
+          {invalid.description && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.description}</p>}
 
           <DatePicker 
           label="Start Date"
           format="YYYY-MM-DD"
           inputFormat="YYYY-MM-DD"
-          value={options.start_date}
-          onChange={(e)=>setOptions({...options, start_date: e.target.value})}
+          value={options.start_date ? options.start_date : invalid.start_date ? dayjs(""): null}
+          onChange={(e)=>setOptions({...options, start_date: e})}
           id="start_date" 
-          className="my-2">
+          className={`my-2`} 
+          error = {invalid.start_date && true}>
             {data && data.start_date}
             </DatePicker>
+            {invalid.start_date && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.start_date}</p>}
             <DatePicker 
           label="End Date"
           format="YYYY-MM-DD"
           inputFormat="YYYY-MM-DD"
-          value={options.end_date}
-          onChange={(e)=>setOptions({...options, end_date: e.target.value})}
+          value={options.end_date && options.end_date}
+          minDate={options.start_date}
+          onChange={(e)=>setOptions({...options, end_date: e})}
           id="end_date" 
           className="my-2">
             {data && data.end_date}
@@ -228,11 +278,15 @@ function ProjectForm({setData, data}){
 
         <SelectSearch table="keyword" add={false} multi={true} list={true}
           options={options} setOptions={setOptions} 
-          data={keywords_list}/>
+          data={keywords_list}
+          error = {invalid.keywords && true}/>
+          {invalid.keywords && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.keywords}</p>}
           
       <SelectSearch table="method" add={false} multi={true} list={true}
           options={options} setOptions={setOptions} 
-          data={methods_list}/>
+          data={methods_list}
+          error = {invalid.methods && true}/>
+          {invalid.methods && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.methods}</p>}
 
       <SelectSearch table="type" add={false} multi={false} list={true}
           options={options} setOptions={setOptions} 
@@ -243,7 +297,9 @@ function ProjectForm({setData, data}){
       <SelectSearch table="person" add={true} multi={true} 
           options={options} setOptions={setOptions} 
           data={persons} parameter={["first_name", "last_name"]}
-          setModalOpened={setModalOpened}/>
+          setModalOpened={setModalOpened}
+          error = {invalid.persons && true}/>
+          {invalid.persons && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.persons}</p>}
 
         <Form.Text muted>
         Add people who are part of this Project
@@ -253,7 +309,9 @@ function ProjectForm({setData, data}){
       <SelectSearch table="group" add={true} multi={true} 
           options={options} setOptions={setOptions} 
           data={groups} parameter="name"
-          setModalOpened={setModalOpened}/>
+          setModalOpened={setModalOpened}
+          error = {invalid.groups && true}/>
+          {invalid.groups && <p className={`text-danger small m-0 mb-2`} style={{textAlign:"left"}}>{invalid.groups}</p>}
 
       <SelectSearch table="partner" add={true} multi={true} 
           options={options} setOptions={setOptions} 
@@ -281,6 +339,11 @@ function ProjectForm({setData, data}){
         <Button className="mt-5" variant="contained" onClick={handleSave}>
             Save
           </Button>
+          {formState != "" &&
+                <p className={`${formState=="fail" ?"text-danger": "text-success"} small`} style={{textAlign:"left"}}>
+              {formState=="fail" ? `${data==null? "Adding": "Updating"} record failed, please confirm your inputs are in correct format and you have filled all required fields`
+              :`${data==null? "Adding": "Updating"} record succeeded`}
+            </p>}
         </>
         
     )
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -16,20 +16,17 @@ const client = axios.create({
 const categories = ["user", "person", "group", "partner", "ressource", "project"]
 
 export const getTableData = createAsyncThunk(`data/getData`, async (table) =>{
-    // let url = new URL(table, fetchingURL);
     const response = await client.get(`${table}/`);
     
     return {data:response.data, category:table}
   })
 
 export const loginAPI = createAsyncThunk(`data/login`, async (cred) =>{
-    // let url = new URL(table, fetchingURL);
     const response = await client.post(`login`, cred)
     
     return response.data
   })
 export const logoutAPI = createAsyncThunk(`data/logout`, async () =>{
-    // let url = new URL(table, fetchingURL);
     const response = await client.post(`logout`);
     
     return response
@@ -37,37 +34,39 @@ export const logoutAPI = createAsyncThunk(`data/logout`, async () =>{
   
   
 
-export const addTableRow = createAsyncThunk(`data/addData`, async ({table, row}) =>{
-  // let url = new URL(table, fetchingURL);
+export const addTableRow = createAsyncThunk(`data/addData`, async ({table, row}, { rejectWithValue }) =>{
 
   try{
     const response = await client.post(`${table}/`, row);
     return {data:response.data, category:table}
-  }catch(err){
-    console.log(err.response.data);
- }
-  
+  }catch (err) {
+    let error = err // cast the error for access
+    if (!error.response) {
+      throw err
+    }
+    // We got validation errors, let's return those so we can reference in our component and set form errors
+    return rejectWithValue(error.response.data)
+  }
   
 })
 
 export const removeTableRow = createAsyncThunk(`data/removeData`, async ({table, rowId}) =>{ 
-  // let url = new URL(table+`/${rowId}/`, fetchingURL);
-  try{
     const response = await client.delete(table+`/${rowId}/`);
     return {rowId:rowId, category:table}
-  }catch(err){
-    console.log(err.response.data);
-  }
 })
 
-export const updateTableRow = createAsyncThunk(`data/updateData`, async ({table, row, rowId}) =>{
-  // let url = new URL(table+`/${rowId}/`, fetchingURL);
+export const updateTableRow = createAsyncThunk(`data/updateData`, async ({table, row, rowId}, { rejectWithValue }) =>{
   
   try{
     const response = await client.patch(table+`/${rowId}/`, row);
     return {data:response.data, rowId:rowId, category:table, row:row}
-  }catch(err){
-    console.log(err.response.data);
+  }catch (err) {
+    let error = err // cast the error for access
+    if (!error.response) {
+      throw err
+    }
+    // We got validation errors, let's return those so we can reference in our component and set form errors
+    return rejectWithValue(error.response.data)
   }
 
 })
