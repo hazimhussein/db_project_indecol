33a5375efc872156e6a877d64baa470f8e426b68
Hazim Hussein <hazimh@ntnu.no>
2024-04-09 17:51:49 +0200

    change user without changing password

--- backend/app_indecol/serializers.py
+++ backend/app_indecol/serializers.py
@@ -40,7 +40,9 @@ class UserSerializer(serializers.ModelSerializer):
         return super(UserSerializer, self).create(validated_data)
     
     def update(self, instance, validated_data):
-        validated_data['password'] = make_password(validated_data.get('password'))
+        if validated_data.get('password') != None:
+            validated_data['password'] = make_password(validated_data.get('password'))
+        
         return super(UserSerializer, self).update(instance, validated_data)
     
     def to_representation(self, instance):
